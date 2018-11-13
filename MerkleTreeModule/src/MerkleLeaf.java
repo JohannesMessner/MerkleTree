ab7fed6ce457d9cbf148059bb33ac12eb632568c
Johannes Messner <messnerjo@gmail.com>
2018-11-13 17:18:10 +0100

    Added Constructors and addValue-Method to add a value to MerkleLeaf

--- MerkleTreeModule/src/MerkleLeaf.java
+++ MerkleTreeModule/src/MerkleLeaf.java
@@ -1,2 +1,22 @@
+import java.util.Optional;
+
 public class MerkleLeaf<V> extends MerkleNode {
+
+  private MerkleInnerNode<V> parent;
+  private Optional<V> value;
+
+  MerkleLeaf(){
+    super();
+    this.value = Optional.empty();
+  }
+
+  MerkleLeaf(MerkleInnerNode<V> parent){
+    this.parent = parent;
+    this.value = Optional.empty();
+  }
+
+  private void setValue(V value){
+    this.value = Optional.of(value);
+    this.hash = Optional.of(value.hashCode());
+  }
 }
