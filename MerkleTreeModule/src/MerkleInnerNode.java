c044870f19ac1006b48aba5bb334b62f72e4be27
Johannes Messner <messnerjo@gmail.com>
2018-11-13 16:46:01 +0100

    Added Constructors and Getters to innerMerkleNode

--- MerkleTreeModule/src/MerkleInnerNode.java
+++ MerkleTreeModule/src/MerkleInnerNode.java
@@ -1,2 +1,33 @@
+import java.util.Optional;
+
 public class MerkleInnerNode<V> extends MerkleNode {
+
+  private MerkleNode<V> left;
+  private MerkleNode<V> right;
+  private MerkleInnerNode<V> parent;
+  private Optional<Long> hash;
+
+  public MerkleInnerNode (){}
+
+  /**
+   * Constructor that sets a Parent-Node
+   *
+   * @param parent MerkleInnerNode that is the Nodes parent
+   */
+  public MerkleInnerNode(MerkleInnerNode<V> parent){
+    this.parent = parent;
+    this.hash = Optional.empty();
+  }
+
+  public MerkleInnerNode<V> getParent() {
+    return parent;
+  }
+
+  public MerkleNode<V> getLeft() {
+    return left;
+  }
+
+  public MerkleNode<V> getRight() {
+    return right;
+  }
 }
