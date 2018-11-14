29f0a9557579c0244fd289ec89f0e17b6b72ac6f
Johannes Messner <messnerjo@gmail.com>
2018-11-14 16:06:33 +0100

    Implemented public methods in HashTree interface.

--- MerkleTreeModule/src/MerkleTree/Hashtree.java
+++ MerkleTreeModule/src/MerkleTree/Hashtree.java
@@ -1,4 +1,16 @@
 package MerkleTree;
 
+import java.util.List;
+
 public interface Hashtree<V> {
+
+  void setHash(int position, long hash);
+
+  void setValue(int positon, V value);
+
+  void clear();
+
+  boolean isConsistent();
+
+  List<Integer> getMissing();
 }
